fb55c32339b0449fec38a881ba888b338c6e56e2
Willy Tarreau <w@1wt.eu>
2020-04-08 10:07:02 +0200

    code: reduce the interline step between large digits
    By saving 2 pixels per large digit, we can actually save a total of 4
    which will allow to add up to 3 lines of descriptions.

--- code/appli.lua
+++ code/appli.lua
@@ -116,11 +116,11 @@ pub_timer:alarm(amb_timer_int,tmr.ALARM_SEMI,function()
   if disp then
     local t = time_now()
     draw_7seg_str(0,0,string.format("%02d:%02d",t["hour"],t["min"]))
-    draw_7seg_str(0,30,string.format("%2.1f°", amb_temp_cur))
-    disp:drawStr(0,60,string.format("%2.1f%% h", amb_humi_cur))
-    if adc_mv then disp:drawStr(0,70,string.format("%1.2f Vbat", amb_volt_cur)) end
-    disp:drawStr(0,80,string.format("mqtt=%s", tostring(conn_states[amb_mqtt_state+1])))
-    disp:drawStr(0,90,string.format("room=%s", tostring(amb_node_room)))
+    draw_7seg_str(0,28,string.format("%2.1f°", amb_temp_cur))
+    disp:drawStr(0,56,string.format("%2.1f%% h", amb_humi_cur))
+    if adc_mv then disp:drawStr(0,66,string.format("%1.2f Vbat", amb_volt_cur)) end
+    disp:drawStr(0,76,string.format("mqtt=%s", tostring(conn_states[amb_mqtt_state+1])))
+    disp:drawStr(0,86,string.format("room=%s", tostring(amb_node_room)))
   end
 
   if disp then disp:sendBuffer() end
